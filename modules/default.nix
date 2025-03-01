30c0608172c5e08713306d0d2c9b319a8b083411
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-01 18:12:24 -0500

    add imports

--- modules/default.nix
+++ modules/default.nix
@@ -2,13 +2,20 @@
   imports = [
     ./adventureworks
     ./airflow
+    ./alice-bitcoin
+    ./alice-specter
     ./argo-events
     ./argo-workflows
+    ./authentik
+    ./cloudbeaver
+    ./demo
     ./forgejo
     ./harbor
+    ./homer
     ./minio
     ./mssql
     ./postgresql
     ./sealed-secrets
+    ./tempo
   ];
 }
