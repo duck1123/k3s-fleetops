ff6d4f3c674f6c393f49cea644f44f18eb419863
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-02 11:02:49 -0500

    alice-lnd

--- modules/alice-lnd/default.nix
+++ modules/alice-lnd/default.nix
@@ -4,9 +4,9 @@ let
 
   chartConfig = {
     repo = "https://chart.kronkltd.net/";
-    chart = "lnd-desktop";
-    version = "0.1.0";
-    chartHash = "sha256-lzGWuSAzOR/n5iBhg25einXA255SwTm0BRB88lUdEoE=";
+    chart = "lnd";
+    version = "0.3.9";
+    chartHash = "sha256-PgA65z/LuWvCfCdTSu9j+CpSS4eWcLY20oSqaBxFklA=";
   };
 
   userEnv = "alice";
@@ -15,39 +15,36 @@ let
   imageVersion = "v1.10.3";
 
   defaultValues = {
-    configurationFile = ''
-lnd.conf"
-  "[Bitcoin]
-bitcoin.active=1
-bitcoin.mainnet=0
-bitcoin.testnet=0
-bitcoin.regtest=1
-bitcoin.node=bitcoind
-
-[Bitcoind]
-bitcoind.rpchost=bitcoin.alice:18443
-bitcoind.rpcuser=rpcuser
-bitcoind.rpcpass=rpcpassword
-bitcoind.zmqpubrawblock=tcp://bitcoin.alice:28332
-bitcoind.zmqpubrawtx=tcp://bitcoin.alice:28333
-
-[Application Options]
-debuglevel=info
-restlisten=0.0.0.0:8080
-rpclisten=0.0.0.0:10009
-tlsextradomain=lnd.alice.svc.cluster.local
-tlsextraip=0.0.0.0
-alias=Node alice
-'';
-    loop.enable = false;
-    pool.enable = false;
-    persistence.enable = false;
     autoUnlock = false;
     autoUnlockPassword = "unlockpassword";
-    network = "regtest";
+    configurationFile."lnd.conf" = ''
+      [Bitcoin]
+      bitcoin.active=1
+      bitcoin.mainnet=0
+      bitcoin.testnet=0
+      bitcoin.regtest=1
+      bitcoin.node=bitcoind
+
+      [Bitcoind]
+      bitcoind.rpchost=bitcoin.alice:18443
+      bitcoind.rpcuser=rpcuser
+      bitcoind.rpcpass=rpcpassword
+      bitcoind.zmqpubrawblock=tcp://bitcoin.alice:28332
+      bitcoind.zmqpubrawtx=tcp://bitcoin.alice:28333
+
+      [Application Options]
+      debuglevel=info
+      restlisten=0.0.0.0:8080
+      rpclisten=0.0.0.0:10009
+      tlsextradomain=lnd.alice.svc.cluster.local
+      tlsextraip=0.0.0.0
+      alias=Node alice
+    '';
     ingress.host = "lnd.alice.demo.dinsro.com";
-
-
+    loop.enable = false;
+    network = "regtest";
+    persistence.enable = false;
+    pool.enable = false;
   };
 
   values = lib.attrsets.recursiveUpdate defaultValues cfg.values;
