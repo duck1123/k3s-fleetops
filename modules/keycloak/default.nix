30b606e84e662b92d814aedb2b69592b88c971d9
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-02 11:02:49 -0500

    keycloak

--- modules/keycloak/default.nix
+++ modules/keycloak/default.nix
@@ -1,20 +1,47 @@
-{ charts, config, lib, ... }:
+{ charts, config, lib, pkgs, ... }:
 let
   cfg = config.services.keycloak;
 
-  chartConfig = {
-    repo = "https://helm.gokeycloak.io";
-    chart = "keycloak";
-    version = "1.16.0";
-    chartHash = "sha256-B1pmsE4zsl8saUnBBzljmJY6Lq6vrVuIeTMStpy3pPc=";
+  chart = lib.helmChart {
+    inherit pkgs;
+    chartTgz = ../../charts/keycloak-24.1.0.tgz;
+    chartName = "keycloak";
   };
 
   defaultNamespace = "keycloak";
   domain = "keycloak.dev.kronkltd.net";
+  adminDomain = "keycloak-admin.dev.kronkltd.net";
 
   clusterIssuer = "letsencrypt-prod";
 
   defaultValues = {
+    auth = {
+      adminUser = "admin";
+      existingSecret = "keycload-admin-password";
+      passwordSecretKey = "password";
+    };
+
+    ingress = {
+      enabled = true;
+      ingressClassName = "traefik";
+      hostname = domain;
+      annotations = {
+        "cert-manager.io/cluster-issuer" = "letsencrypt-prod";
+        "ingress.kubernetes.io/force-ssl-redirect" = "true";
+      };
+      tls = true;
+    };
+
+    adminIngress = {
+      enabled = true;
+      ingressClassName = "traefik";
+      hostname = adminDomain;
+      annotations = {
+        "cert-manager.io/cluster-issuer" = "letsencrypt-prod";
+        "ingress.kubernetes.io/force-ssl-redirect" = "true";
+      };
+      tls = true;
+    };
   };
 
   values = lib.attrsets.recursiveUpdate defaultValues cfg.values;
@@ -36,8 +63,7 @@ in with lib; {
   };
 
   config = mkIf cfg.enable {
-    applications.keycloak = let chart = helm.downloadHelmChart chartConfig;
-    in {
+    applications.keycloak = {
       inherit namespace;
       createNamespace = true;
       helm.releases.keycloak = { inherit chart values; };
