e775885402763b61d1075197aab3863f3e15bdae
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-03-16 11:17:49 -0400

    Downgrade to v0.2.1

--- modules/crossplane/providers/digital-ocean/default.nix
+++ modules/crossplane/providers/digital-ocean/default.nix
@@ -36,7 +36,7 @@ in with lib; {
         providers.provider-upjet-digitalocean = {
           metadata = { inherit namespace; };
           spec.package =
-            "crossplane-contrib/provider-upjet-digitalocean:v0.3.0";
+            "crossplane-contrib/provider-upjet-digitalocean:v0.2.1";
         };
 
         # providerConfigs.do-provider.spec.credentials = {
