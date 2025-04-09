c44406b51401119979ef6b7b1b3559b09d58d914
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-09 16:56:52 -0400

    minimal

--- env/dev.nix
+++ env/dev.nix
@@ -16,79 +16,28 @@ in {
   };
 
   services = {
-    adventureworks.enable = false;
-    airflow.enable = false;
-
-    # TODO: I know we can do better than this
-    alice-bitcoin.enable = false;
-    alice-lnd.enable = false;
-    alice-specter.enable = false;
-
     argocd.enable = true;
-    argo-events.enable = false;
-    argo-workflows.enable = false;
-
-    authentik.enable = false;
     cert-manager.enable = true;
-
     cloudbeaver.enable = true;
-
-    crossplane = {
-      enable = false;
-      providers.digital-ocean.enable = false;
-    };
-
-    demo.enable = false;
-    dinsro.enable = false;
-    forgejo.enable = false;
-    harbor.enable = false;
-    homer.enable = false;
 
     jupyterhub = {
       enable = true;
       domain = "jupyterhub.${base-domain}";
-      # domain = "jupyterhub.localhost";
       ssl = true;
       inherit (secrets.jupyterhub) cookieSecret cryptkeeperKeys password proxyToken;
     };
 
-    keycloak.enable = false;
-
-    kyverno.enable = false;
-
-    lldap.enable = false;
-
-    longhorn.enable = false;
-
-    memos.enable = false;
-    metabase.enable = false;
-    mindsdb.enable = false;
-
     minio = {
       api-domain = "minio-api.${base-domain}";
-      # api-domain = "minio-api.localtest.me";
       domain = "minio.${base-domain}";
-      # domain = "minio.localtest.me";
       enable = true;
       tls.enable = true;
     };
 
-    mssql.enable = false;
-    openldap.enable = false;
-    opentelemetry-collector.enable = false;
     postgresql.enable = true;
-    redis.enable = false;
     sealed-secrets.enable = true;
-
     sops.enable = true;
-
-    spark = {
-      enable = true;
-      # domain = "spark.${base-domain}";
-    };
-
-    sqlpad.enable = false;
-    tempo.enable = false;
+    spark.enable = true;
     traefik.enable = true;
   };
 }
