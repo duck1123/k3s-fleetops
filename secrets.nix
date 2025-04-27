04754e8cdac50aaba689ff940127b22e4d3b93f9
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-27 13:25:04 -0400

    needed secrets and chart options

--- secrets.nix
+++ secrets.nix
@@ -1,4 +1,14 @@
-{
+{config, lib, pkgs, ... }: {
+  imports = [
+    ./secrets-module.nix
+  ];
+
+  secrets = {
+    "nocodb.minio.bucketName" = {
+
+    };
+  };
+
   nocodb = {
     minio = {
       bucketName = {
