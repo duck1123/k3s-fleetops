980e13e4a003aea5aebd5f7aedd5ec1db67ede91
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-27 13:25:04 -0400

    needed secrets and chart options

---
+++ secrets-mapping.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+with lib;
+let
+  appSecrets = appName: neededSecrets:
+    listToAttrs (map (key: {
+      name = key;
+      value = "${appName}/${key}";
+    }) neededSecrets);
+in {
+  config = {
+    services = mapAttrs (appName: appCfg: {
+      inherit (appCfg) enable neededSecrets;
+      secrets = mkIf appCfg.enable (appSecrets appName appCfg.neededSecrets);
+    }) config.services;
+
+    # Optional: Validate that secrets exist
+    assertions = map (app: {
+      assertion = all (key: app.secrets ? key) app.neededSecrets;
+      message = "Service ${appName} is missing required secrets!";
+    }) (attrValues config.services);
+  };
+}
