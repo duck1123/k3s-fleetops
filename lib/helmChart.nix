c0dea0975bad077b0a96983bbcdb6b8c1e199eee
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-04-27 13:25:04 -0400

    needed secrets and chart options

--- lib/helmChart.nix
+++ lib/helmChart.nix
@@ -1,5 +1,4 @@
 { pkgs, chartTgz, chartName }:
-
 pkgs.runCommand chartName { inherit chartTgz; } ''
   mkdir -p $out
   tar -xzf ${chartTgz} -C $out --strip-components=1
---
+++ secrets-module.nix
@@ -0,0 +1,27 @@
+{ lib, ... }:
+with lib; {
+  options = {
+    secrets = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          keepassPath = mkOption {
+            type = types.listOf types.str;
+            description = "Path inside Keepass database to the secret.";
+          };
+          description = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = "Optional description of what this secret is for.";
+          };
+          env = mkOption {
+            type = types.nullOr
+              (types.enum [ "production" "staging" "development" ]);
+            default = null;
+            description = "Environment tag, if needed.";
+          };
+        };
+      });
+      description = "Secrets definition mapping.";
+    };
+  };
+}
