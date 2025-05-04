cc050466459f8da124c0e3731cc4a39311b0d5e8
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-05-04 12:49:28 -0400

    Switch to nixhelm for cert-manager's chart

--- modules/cert-manager/default.nix
+++ modules/cert-manager/default.nix
@@ -1,15 +1,8 @@
-{ config, lib, ... }:
+{ charts, config, lib, ... }:
 with lib;
 mkArgoApp { inherit config lib; } {
   name = "cert-manager";
-
   # https://artifacthub.io/packages/helm/cert-manager/cert-manager
-  chart = lib.helm.downloadHelmChart {
-    repo = "https://charts.jetstack.io";
-    chart = "cert-manager";
-    version = "v1.17.2";
-    chartHash = "sha256-8d/BPet3MNGd8n0r5F1HEW4Rgb/UfdtwqSFuUZTyKl4=";
-  };
-
+  chart = charts.jetstack.cert-manager;
   defaultValues = cfg: { crds.enabled = true; };
 }
