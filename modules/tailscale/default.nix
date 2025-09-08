4fa7fcc078303d030fb1945c172aea1e8d89037c
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-09-07 23:52:30 -0400

    tailscale

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -1,40 +1,16 @@
 { ageRecipients, config, lib, pkgs, ... }:
-let
-  app-name = "tailscale";
-  cfg = config.services.${app-name};
+with lib;
+mkArgoApp { inherit config lib; } {
+  name = "tailscale";
 
-  # https://github.com/tailscale/tailscale/blob/main/cmd/k8s-operator/deploy/chart/values.yaml
-  values = lib.attrsets.recursiveUpdate { } cfg.values;
-in with lib; {
-  options.services.${app-name} = {
-    chart = mkOption {
-      type = types.path;
-      default = lib.helm.downloadHelmChart {
-        repo = "https://pkgs.tailscale.com/helmcharts";
-        chart = "tailscale-operator";
-        version = "1.82.0";
-        chartHash = "sha256-8b9h+ZAls2FHU6fy4mKn+yR4o/p2BYtSWbaBv5BXjvE=";
-      };
-      description = ''
-        Optional Helm chart derivation to use for deploying this app.
-        Should point to a path produced by something like `lib.helm.downloadHelmChart`.
-      '';
-    };
+  chart = helm.downloadHelmChart {
+    repo = "https://pkgs.tailscale.com/helmcharts";
+    chart = "tailscale-operator";
+    version = "1.82.0";
+    chartHash = "sha256-8b9h+ZAls2FHU6fy4mKn+yR4o/p2BYtSWbaBv5BXjvE=";
+  };
 
-    enable = mkEnableOption "Enable application";
-
-    namespace = mkOption {
-      description = mdDoc "The namespace to install into";
-      type = types.str;
-      default = app-name;
-    };
-
-    neededSecrets = mkOption {
-      type = types.listOf types.str;
-      default = [];
-      description = "List of required secret keys (names, no paths).";
-    };
-
+  extraOptions = {
     oauth = {
       # https://tailscale.com/kb/1185/kubernetes
       authKey = mkOption {
@@ -42,55 +18,39 @@ in with lib; {
         type = types.str;
         default = "";
       };
+
       clientId = mkOption {
         description = mdDoc "The client id";
         type = types.str;
         default = "";
       };
+
       clientSecret = mkOption {
         description = mdDoc "The client secret";
         type = types.str;
         default = "";
       };
     };
-
-    values = mkOption {
-      description = "All the values";
-      type = types.attrsOf types.anything;
-      default = { };
-    };
   };
 
-  config = mkIf cfg.enable {
-    applications.${app-name} = {
-      inherit (cfg) namespace;
-      createNamespace = true;
-      finalizers = [ "resources-finalizer.argocd.argoproj.io" ];
-      helm.releases.${app-name} = {
-        inherit values;
-        inherit (cfg) chart;
+  extraResources = cfg: {
+    sopsSecrets = {
+      tailscale-auth = createSecret {
+        inherit ageRecipients lib pkgs;
+        inherit (cfg) namespace;
+        secretName = "tailscale-auth";
+        values = with cfg.oauth; { TS_AUTHKEY = authKey; };
       };
 
-      resources.sopsSecrets = {
-        tailscale-auth = lib.createSecret {
-          inherit ageRecipients lib pkgs;
-          inherit (cfg) namespace;
-          secretName = "tailscale-auth";
-          values = with cfg.oauth; { TS_AUTHKEY = authKey; };
-        };
-
-        operator-oauth = lib.createSecret {
-          inherit ageRecipients lib pkgs;
-          inherit (cfg) namespace;
-          secretName = "operator-oauth";
-          values = with cfg.oauth; {
-            client_id = clientId;
-            client_secret = clientSecret;
-          };
+      operator-oauth = lib.createSecret {
+        inherit ageRecipients lib pkgs;
+        inherit (cfg) namespace;
+        secretName = "operator-oauth";
+        values = with cfg.oauth; {
+          client_id = clientId;
+          client_secret = clientSecret;
         };
       };
-
-      syncPolicy.finalSyncOpts = [ "CreateNamespace=true" ];
     };
   };
 }
