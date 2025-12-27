15f384d8aa5dbe2501c08ca0fefe49cba322e221
Duck Nebuchadnezzar <duck@kronkltd.net>
2025-12-27 09:43:16 -0500

    limit

--- modules/gluetun/default.nix
+++ modules/gluetun/default.nix
@@ -62,7 +62,7 @@ mkArgoApp { inherit config lib; } rec {
               serviceAccountName = "default";
               dnsPolicy = "None";
               dnsConfig = {
-                nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+                nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" ];
                 searches = [ ];
                 options = [
                   {
