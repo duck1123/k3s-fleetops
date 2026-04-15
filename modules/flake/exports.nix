5959ffb1ecbe1d2e729890abf4e0054995e1c43a
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-04-15 19:07:38 -0400

    export flake module

---
+++ modules/flake/exports.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  # Export a flakeModule that dotfiles (or any other consumer) can import to get:
+  #   - flake.lib option + utility functions (loadSecrets, fromYAML, toYAML, mkArgoApp, waitForGluetun)
+  #   - flake.nixidyApps option + all application modules
+  #   - flake.modules.generic.ageRecipients module
+  flake.flakeModules.default = {
+    imports = [
+      ./flake-parts.nix
+      ../lib/fromYAML.nix
+      ../lib/toYAML.nix
+      ../lib/loadSecrets.nix
+      ../lib/mkArgoApp.nix
+      ../lib/waitForGluetun.nix
+      ../applications.nix
+      ../sops.nix
+    ];
+  };
+}
