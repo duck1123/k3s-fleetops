e48ce7c794196125dd42dd9897d7ea2f85c94d92
Duck Nebuchadnezzar <duck@kronkltd.net>
2026-04-18 12:19:21 -0400

    change nocodb deployment strategy

--- applications/nocodb.nix
+++ applications/nocodb.nix
@@ -198,6 +198,8 @@
             spec = {
               replicas = cfg.replicas;
 
+              strategy.type = "Recreate";
+
               selector.matchLabels = {
                 "app.kubernetes.io/instance" = name;
                 "app.kubernetes.io/name" = name;
